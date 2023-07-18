2c5912370218ecc735036580bda3dee2649c6cb4
Aleksandr8I22 <asf47@tpu.ru>
2023-07-19 03:42:05 +0500

    Pt_3_7: dictionary with True\False values

---
+++ Py_3_7.py
@@ -0,0 +1,3 @@
+a = str(input("Введите строчку (Можно с пробелами): ")).replace(" ", "")
+vse_gls = ["а", "е", "ё", "и", "о", "у", "ы", "э", "ю", "я", "e", "y", "u", "i", "o", "a"]
+print([{a[i]: True if a[i] in vse_gls else False} for i in range(len(a)) if a[i].isalpha()])
