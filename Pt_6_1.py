f91ab9802f0bb67c9e20ef9769fef9bdb45ce66d
Aleksandr8I22 <asf47@tpu.ru>
2023-07-19 23:32:23 +0500

    Pt_6_1: parser of music by any artist (if the link is correct) from Yandex Music

---
+++ Pt_6_1.py
@@ -0,0 +1,24 @@
+from bs4 import BeautifulSoup
+import requests
+
+url = input("Введите ссылку: ")
+try:
+    page = requests.get(url)
+    if page.status_code == 200:
+        soup = BeautifulSoup(page.text, "html.parser")
+        Top10 = soup.findAll("a", class_="d-track__title deco-link deco-link_stronger")[0:10]
+        if Top10 is not None:
+            print("Топ песен: ")
+            for data in Top10:
+                print(data.text)
+        else:
+            print("Неверная ссылка")
+    elif page.status_code == 404:
+        print("Нет такой страницы")
+    else:
+        print("ЧтО?")
+except requests.exceptions.MissingSchema:
+    print("Непрввильная ссылка на яндекс музыку в разделе Треки")
+
+# https://music.yandex.ru/artist/257461/tracks
+# https://music.yandex.ru/artist/165131/tracks
