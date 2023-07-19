1f38492d5e1fedfd218ba8b6be1f3d322f7bc35d
Aleksandr8I22 <asf47@tpu.ru>
2023-07-19 05:40:31 +0500

    Pt_4_3: outputs all prime numbers in the range

---
+++ Pt_4_3.py
@@ -0,0 +1,15 @@
+import math
+
+a, b = math.ceil(float(input("Введите левую границу: "))), math.floor(float(input("Введите правую границу: ")))
+
+
+def Postie(o):
+    if o == 2:
+        return True
+    for i in range(2,  math.ceil(o**0.5)+1):
+        if int(o/i) == o/i:
+            return False
+    return True
+
+
+print([i for i in range(a, b + 1) if Postie(i) if i != 1])
