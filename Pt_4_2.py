e41b25178cb1aaa088525d6ee1be4ac6d1edd8c6
Aleksandr8I22 <asf47@tpu.ru>
2023-07-19 05:39:06 +0500

    Pt_4_2: finds all palindromes of given string

---
+++ Pt_4_2.py
@@ -0,0 +1,17 @@
+Stroka = str(input("Введите строку: "))
+Polidroms = set()
+
+def Poisk(a, low, high):
+    result = []
+    while (low >= 0) and (high < len(a)) and (a[low] == a[high]):
+        result.append(a[low:high+1])
+        low -= 1
+        high += 1
+    return result
+
+
+for i in range(len(Stroka)):
+    Polidroms.update((Poisk(Stroka, i, i+1)))
+    Polidroms.update((Poisk(Stroka, i, i)))
+
+print(Polidroms)
